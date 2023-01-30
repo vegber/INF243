d8c453ff7e6ee5501c2f1671896028c13f7c5948
Vegard Berge <Vegard.Berge@knowit.no>
2023-01-30 15:49:21 +0100

    aødlsjf

---
+++ src/BSC/BSC_capasity.py
@@ -0,0 +1,8 @@
+"""
+Suppose there are two binary symmetric channels with bit error prob.
+    p_1 = 0.1 
+    p_2 = 0.01 
+Calculate the capasity of these two BSCs and justify 
+    which one has better channel capasity!
+"""
+
